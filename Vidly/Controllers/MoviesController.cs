da6135ce332b49ecc6274549225c15c4f9a26f51
Ragavan Rajan <rr100@students.waikato.ac.nz>
2018-10-30 21:29:00 +1300

    Display the list of customers and movies
    Display the list of customers and movies as table using Linq

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -10,14 +7,30 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Leon the professional!"};
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -25,36 +38,8 @@ namespace Vidly.Controllers
                 Movie = movie,
                 Customers = customers
             };
-//            return View(movie);
-//            return Content("Hello World");
-//            return HttpNotFound();
-//            return new EmptyResult();  // returns empty result 
-//            return RedirectToAction("Index", "Home", new{page =1, sortBy = "name"});
+
             return View(viewModel);
-
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("Id = " + id);
-        }
-
-        //movies 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("PageIndex = {0}&sortyBy={1}", pageIndex, sortBy));
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
         }
     }
-
-    
 }
